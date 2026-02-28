a6269bdeabcf69a0dd6d0c37644f793fc1877b5f
Aditya-Raj143 <adityaxraj72004@gmail.com>
2026-02-28 17:29:02 +0530

    mcp server connection function added

--- apps/agent/src/index.ts
+++ apps/agent/src/index.ts
@@ -28,4 +28,41 @@ class MCPClient {
     this.mcp = new Client({ name: 'mcp-client-cli', version: '1.0.0' });
   }
   // methods will go here
+
+  async connectToServer(serverScriptPath: string) {
+    try {
+      const isJs = serverScriptPath.endsWith('.js');
+      const isPy = serverScriptPath.endsWith('.py');
+      if (!isJs && !isPy) {
+        throw new Error('Server script must be a .js or .py file');
+      }
+      const command = isPy
+        ? process.platform === 'win32'
+          ? 'python'
+          : 'python3'
+        : process.execPath;
+
+      this.transport = new StdioClientTransport({
+        command,
+        args: [serverScriptPath],
+      });
+      await this.mcp.connect(this.transport);
+
+      const toolsResult = await this.mcp.listTools();
+      this.tools = toolsResult.tools.map((tool) => {
+        return {
+          name: tool.name,
+          description: tool.description,
+          input_schema: tool.inputSchema,
+        };
+      });
+      console.log(
+        'Connected to server with tools:',
+        this.tools.map(({ name }) => name),
+      );
+    } catch (e) {
+      console.log('Failed to connect to MCP server: ', e);
+      throw e;
+    }
+  }
 }
