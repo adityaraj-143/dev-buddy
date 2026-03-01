49f44578246977ac872a7b832a196351d499ce50
Aditya-Raj143 <adityaxraj72004@gmail.com>
2026-03-02 00:14:35 +0530

    implement processQuery method to handle user queries and tool interactions

--- apps/agent/src/index.ts
+++ apps/agent/src/index.ts
@@ -65,4 +65,56 @@ class MCPClient {
       throw e;
     }
   }
+
+  async processQuery(query: string) {
+    const messages: MessageParam[] = [
+      {
+        role: 'user',
+        content: query,
+      },
+    ];
+
+    const response = await this.anthropic.messages.create({
+      model: 'claude-sonnet-4-20250514',
+      max_tokens: 1000,
+      messages,
+      tools: this.tools,
+    });
+
+    const finalText = [];
+
+    for (const content of response.content) {
+      if (content.type === 'text') {
+        finalText.push(content.text);
+      } else if (content.type === 'tool_use') {
+        const toolName = content.name;
+        const toolArgs = content.input as { [x: string]: unknown } | undefined;
+
+        const result = await this.mcp.callTool({
+          name: toolName,
+          arguments: toolArgs,
+        });
+        finalText.push(
+          `[Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}]`,
+        );
+
+        messages.push({
+          role: 'user',
+          content: result.content as string,
+        });
+
+        const response = await this.anthropic.messages.create({
+          model: 'claude-sonnet-4-20250514',
+          max_tokens: 1000,
+          messages,
+        });
+
+        finalText.push(
+          response.content[0].type === 'text' ? response.content[0].text : '',
+        );
+      }
+    }
+
+    return finalText.join('\n');
+  }
 }
